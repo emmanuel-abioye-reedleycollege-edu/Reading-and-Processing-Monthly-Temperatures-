e0498e9db40adf740c3df45aba17d5b011210536
Abiodun Emmanuel Abioye <emmanuel.abioye@reedleycollege.edu>
2025-03-17 15:35:31 -0700

    Add files via upload

---
+++ 8.5 Arrays Lab - Version Control.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+
+// Step 1: Define the Struct in the global section
+
+
+// Function prototypes
+
+
+int main() {
+
+    return 0;
+}
+
+// Step 2: Read Data into an Array of Structs
+
+
+// Step 3: Print Data
+
+// Step 4: Compute the Average Temperature
+
+
